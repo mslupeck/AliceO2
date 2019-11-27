91009b0ae702912d64b145ce094b4199a5c2a005
arvindkhuntia <akhuntia@cern.ch>
2019-11-27 12:37:09 +0100

    linker fixed

--- DataFormats/Detectors/FIT/FV0/src/DataFormatsFV0LinkDef.h
+++ DataFormats/Detectors/FIT/FV0/src/DataFormatsFV0LinkDef.h
@@ -19,6 +19,8 @@
 
 #pragma link C++ class o2::fv0::MCLabel + ;
 #pragma link C++ class vector < o2::fv0::MCLabel> + ;
+#include "SimulationDataFormat/MCTruthContainer.h"
+#pragma link C++ class o2::dataformats::MCTruthContainer < o2::fv0::MCLabel > + ;
 
 #pragma link C++ class o2::fv0::Digit + ;
 #pragma link C++ class o2::fv0::ChannelData + ;
